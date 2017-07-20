dd14ddf8f9a4477c3af53cb397e469bffea55b06
dzhitomirsky-sc <dzhitomirsky@scand.com>
2017-07-20 16:49:04 +0300

    Refactored InvoiceDetails.react.js to pure render function.

--- src/client/components/MyTasks/details/InvoiceDetails.react.js
+++ src/client/components/MyTasks/details/InvoiceDetails.react.js
@@ -3,26 +3,17 @@ import { formattedTotalSum } from '../../../utils/MathUtils';
 import InvoiceForm from '../../InvoiceReceiptEditor/components/InvoiceEditor/InvoiceForm.react';
 import InvoiceItemsPricePanel from '../../InvoiceReceiptEditor/components/InvoiceEditor/InvoiceItemsPricePanel.react';
 
-export default class InvoiceDetails extends Component {
+const InvoiceDetails = (
+  { invoice },
+  { i18n, termsOfDelivery, termsOfPayment, methodsOfPayment, currencies }
+) => {
 
-  static propTypes = {
-    invoice: PropTypes.object.isRequired
-  };
-
-  static contextTypes = {
-    i18n: PropTypes.object.isRequired,
-    termsOfDelivery: PropTypes.array.isRequired,
-    termsOfPayment: PropTypes.array.isRequired,
-    methodsOfPayment: PropTypes.array.isRequired,
-    currencies: PropTypes.array.isRequired
-  };
-
-  calculateItemsPrice(invoice) {
+  const calculateItemsPrice = () => {
     let totalNetPrice, totalTaxAmount, totalGrossPrice;
     if (invoice.items.length > 0) {
-      totalNetPrice = formattedTotalSum(this.context.i18n, invoice.items, 'totalNetPrice');
-      totalTaxAmount = formattedTotalSum(this.context.i18n, invoice.items, 'taxAmount');
-      totalGrossPrice = formattedTotalSum(this.context.i18n, invoice.items, 'totalGrossPrice');
+      totalNetPrice = formattedTotalSum(i18n, invoice.items, 'totalNetPrice');
+      totalTaxAmount = formattedTotalSum(i18n, invoice.items, 'taxAmount');
+      totalGrossPrice = formattedTotalSum(i18n, invoice.items, 'totalGrossPrice');
     }
     return {
       currency: invoice.currencyId,
@@ -30,36 +21,48 @@ export default class InvoiceDetails extends Component {
       totalTaxAmount: isNaN(totalTaxAmount) ? 0 : totalTaxAmount,
       totalGrossPrice: isNaN(totalGrossPrice) ? 0 : totalGrossPrice
     }
-  }
+  };
 
-  render() {
-    const { invoice } = this.props;
-    return (
-      <div id="oc-invoices-my-tasks-wide-invoice-details">
-        <InvoiceForm
-          readOnly={true}
-          invoice={invoice}
-          customer={invoice.customer}
-          supplier={invoice.supplier}
-          supplierAddresses={invoice.supplierAddresses}
-          supplierContacts={invoice.supplierContacts}
+  return (
+    <div id="oc-invoices-my-tasks-wide-invoice-details">
+      <InvoiceForm
+        readOnly={true}
+        invoice={invoice}
+        customer={invoice.customer}
+        supplier={invoice.supplier}
+        supplierAddresses={invoice.supplierAddresses}
+        supplierContacts={invoice.supplierContacts}
 
-          termsOfDelivery={this.context.termsOfDelivery}
-          termsOfPayment={this.context.termsOfPayment}
-          methodsOfPayment={this.context.methodsOfPayment}
-          currencies={this.context.currencies}
+        termsOfDelivery={termsOfDelivery}
+        termsOfPayment={termsOfPayment}
+        methodsOfPayment={methodsOfPayment}
+        currencies={currencies}
 
-          statusLabel={statusId => statusId}
-          onSave={() => {}}
-          displayMode={'one-column'}
-        />
-        <InvoiceItemsPricePanel
-          readOnly={true}
-          priceInfo={this.calculateItemsPrice(invoice)}
-          onAddPositions={() => {}}
-        />
-      </div>
-    )
-  }
+        statusLabel={statusId => statusId}
+        onSave={() => {
+        }}
+        displayMode={'one-column'}
+      />
+      <InvoiceItemsPricePanel
+        readOnly={true}
+        priceInfo={calculateItemsPrice()}
+        onAddPositions={() => {
+        }}
+      />
+    </div>
+  )
+};
 
-}
+InvoiceDetails.propTypes = {
+  invoice: PropTypes.object.isRequired
+};
+
+InvoiceDetails.contextTypes = {
+  i18n: PropTypes.object.isRequired,
+  termsOfDelivery: PropTypes.array.isRequired,
+  termsOfPayment: PropTypes.array.isRequired,
+  methodsOfPayment: PropTypes.array.isRequired,
+  currencies: PropTypes.array.isRequired
+};
+
+export default InvoiceDetails;
