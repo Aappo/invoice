ac32340f885d9f2a0866ed62a062e963e3eddac2
levchuk <anton.levchuk@jcatalog.com>
2017-08-11 17:37:05 +0300

    #14 Updated old invoice application. Removed unused components.

--- src/client/containers/InvoiceApplication.react.js
+++ src/client/containers/InvoiceApplication.react.js
@@ -2,17 +2,14 @@ import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router';
 import {KeyInRoutes} from '../routes';
 import Router from 'react-router/lib/Router';
-import I18nContext from '../components/util/I18nContext.react';
-import UserDataContext from '../components/util/UserDataContext.react';
+import InitialContextProvider from '../components/util/InitialContextProvider.react';
 
-const InvoiceApplication = ({ locale, formatPatterns, userData }) => (
-  <I18nContext locale={locale} formatPatterns={formatPatterns}>
-    <UserDataContext userData={userData}>
-      <Router history={browserHistory}>
-        {KeyInRoutes()}
-      </Router >
-    </UserDataContext>
-  </I18nContext>
+const InvoiceApplication = (props) => (
+  <InitialContextProvider {...props}>
+    <Router history={browserHistory}>
+      {KeyInRoutes()}
+    </Router >
+  </InitialContextProvider>
 );
 
 InvoiceApplication.propTypes = {
