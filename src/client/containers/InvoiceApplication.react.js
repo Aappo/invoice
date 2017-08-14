72adcaae4da34e8248548e43690385712e96b41c
dzhitomirsky-sc <dzhitomirsky@scand.com>
2017-08-14 16:37:03 +0300

    #56 First obsolete code removal.

--- src/client/containers/InvoiceApplication.react.js
+++ src/client/containers/InvoiceApplication.react.js
@@ -1,18 +1,22 @@
 import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router';
-import {KeyInRoutes} from '../routes';
 import Router from 'react-router/lib/Router';
-// import InitialContextProvider from '../components/util/InitialContextProvider.react';
+import UserDataProvider from '../components/util/UserDataProvider.react';
+import I18nProvider from '../components/util/I18nProvider.react';
+import { ApprovalRoutes } from '../routes';
 
 const InvoiceApplication = (props) => (
-    <Router history={browserHistory}>
-      {KeyInRoutes()}
-    </Router >
+  <UserDataProvider userData={props.userData}>
+    <I18nProvider formatPatterns={props.formatPatterns}>
+      <Router history={browserHistory}>
+        {ApprovalRoutes()}
+      </Router >
+    </I18nProvider>
+  </UserDataProvider>
 );
 
 InvoiceApplication.propTypes = {
-  locale: PropTypes.string.isRequired,
-  formatPatterns: React.PropTypes.object
+  formatPatterns: React.PropTypes.object.isRequired
 };
 
 export default InvoiceApplication;
--- src/server/db/migrations/0.main.js
+++ src/server/db/migrations/0.main.js
@@ -1,47 +1,12 @@
 'use strict';
 
-const Promise = require('bluebird');
-const createInvoiceReceipt = require('./invoice-migration/createInvoiceReceipt');
-const createInvoiceReceiptItem = require('./invoice-migration/createInvoiceReceiptItem');
-const createCostDistribution = require('./invoice-migration/createCostDistribution');
-const createCostDistributionPos = require('./invoice-migration/createCostDistributionPos');
+const createPurchaseInvoice = require('./purchase-invoice/createPurchaseInvoice');
+const createPurchaseInvoiceItem = require('./purchase-invoice/createPurchaseInvoiceItem');
 
-/**
- * Applies migrations for databse tables and data.
- * If all migrations were successul, this method will never be executed again.
- * To identify which migrations have successfully been processed, a migration's filename is used.
- *
- * @param {object} data - [Sequelize]{@link https://github.com/sequelize/sequelize} instance.
- * @param {object} config - A model property for database models and everything from [config.data]{@link https://github.com/OpusCapitaBusinessNetwork/db-init} passed when running the db-initialization.
- * @returns {Promise} [Promise]{@link http://bluebirdjs.com/docs/api-reference.html}
- * @see [Applying data migrations]{@link https://github.com/OpusCapitaBusinessNetwork/db-init#applying-data-migrations}
- */
 module.exports.up = function(db, config) {
-  return createCostDistribution(db.getQueryInterface()).then(() => {
-    return createCostDistributionPos(db.getQueryInterface());
-  }).then(() => {
-    return createInvoiceReceipt(db.getQueryInterface());
-  }).then(() => {
-    return createInvoiceReceiptItem(db.getQueryInterface());
-  })
+  return createPurchaseInvoice(db.getQueryInterface()).then(() => createPurchaseInvoiceItem(db.getQueryInterface()));
 };
 
-/**
- * Reverts all migrations for databse tables and data.
- * If the migration process throws an error, this method is called in order to revert all
- * changes made by the up() method.
- *
- * @param {object} data - [Sequelize]{@link https://github.com/sequelize/sequelize} instance.
- * @param {object} config - A model property for database models and everything from [config.data]{@link https://github.com/OpusCapitaBusinessNetwork/db-init} passed when running the db-initialization.
- * @returns {Promise} [Promise]{@link http://bluebirdjs.com/docs/api-reference.html}
- * @see [Applying data migrations]{@link https://github.com/OpusCapitaBusinessNetwork/db-init#applying-data-migrations}
- */
 module.exports.down = function(db, config) {
-  return Promise.all([
-    db.getQueryInterface().dropTable('InvoiceReceiptItem'),
-    db.getQueryInterface().dropTable('InvoiceReceipt'),
-    db.getQueryInterface().dropTable('CostDistributionPos'),
-    db.getQueryInterface().dropTable('CostDistribution'),
-    db.getQueryInterface().dropTable('CostObject'),
-  ]);
+  return db.getQueryInterface().dropTable('PurchaseInvoiceItem').then(() => db.getQueryInterface().dropTable('PurchaseInvoice'));
 };
