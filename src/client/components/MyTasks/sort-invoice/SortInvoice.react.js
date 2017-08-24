03a0310c24949679fd59c6395228a13eb9f36d0e
levchuk <anton.levchuk@jcatalog.com>
2017-08-24 19:03:21 +0300

    #85 Migrated to @opuscapita/react-select.

--- src/client/components/MyTasks/sort-invoice/SortInvoice.react.js
+++ src/client/components/MyTasks/sort-invoice/SortInvoice.react.js
@@ -1,20 +1,22 @@
 import React, { PropTypes } from 'react';
-import Select from 'react-select';
+import Select from '@opuscapita/react-select';
 import './SortInvoice.less';
-import 'react-select/dist/react-select.css';
 
 
 function SortInvoice({ label, value, items, onChange, ...props }) {
+  console.log(Select)
   return (
     <div className="oc-invoices-sort-invoice" {...props}>
       <span className="oc-invoices-sort-invoice-label">Sort by</span>
-      <Select
-        className="oc-invoices-sort-invoice-select"
-        value={value}
-        searchable={false}
-        options={items}
-        onChange={onChange}
-      />
+      <div className="oc-invoices-sort-invoice-select">
+        <Select
+          value={value}
+          searchable={false}
+          clearable={false}
+          options={items}
+          onChange={onChange}
+        />
+      </div>
     </div>
   );
 }
