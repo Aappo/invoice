bdb90c37ec764a74572b3bb8d867bacfc1d182a4
Daniel Zhitomirsky <dzhitomirsky-sc@users.noreply.github.com>
2017-08-31 15:20:17 +0300

    #104 Added due date related badges. (#112)

--- src/client/components/MyTasks/helpers/UIHelpers.react.js
+++ src/client/components/MyTasks/helpers/UIHelpers.react.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Icon } from '@opuscapita/react-icons';
 
 import {
-  APPROVALACTIONKIND,
+  APPROVAL_STATUS,
+  INVOICE_FLAG
 } from '../constants';
 
 
@@ -21,25 +22,25 @@ class UiHelpers {
     let indicator = '';
 
     switch (status) {
-      case APPROVALACTIONKIND.Approved:
+      case APPROVAL_STATUS.Approved:
         indicator = 'ok';
         break;
-      case APPROVALACTIONKIND.Inspected:
+      case APPROVAL_STATUS.Inspected:
         indicator = 'inspected';
         break;
-      case APPROVALACTIONKIND.InspectionRequired:
+      case APPROVAL_STATUS.InspectionRequired:
         indicator = 'flagged';
         break;
-      case APPROVALACTIONKIND.InspectedAndApproved:
+      case APPROVAL_STATUS.InspectedAndApproved:
         indicator = 'inspectedAndApproved';
         break;
-      case APPROVALACTIONKIND.InspectorClarificationRequired:
+      case APPROVAL_STATUS.InspectorClarificationRequired:
         indicator = 'inClarification';
         break;
-      case APPROVALACTIONKIND.ApproverClarificationRequired:
+      case APPROVAL_STATUS.ApproverClarificationRequired:
         indicator = 'inClarification';
         break;
-      case APPROVALACTIONKIND.Commented:
+      case APPROVAL_STATUS.Commented:
         indicator = 'commented';
         break;
       default:
@@ -92,53 +93,39 @@ class UiHelpers {
 
   /**
    * Get icon for invoice flag
-   * @param {string} flag - Status flag (INVOICESTATUSFLAG).
+   * @param {string} flag - Status flag (INVOICE_FLAG).
    * @param {object} size - Icon size i.e. width and height.
    */
-  // getIconForInvoiceFlag = (flag, size) => {
-  //   return null;
-  //   let indicator = '';
-  //
-  //   switch (flag) {
-  //     case INVOICESTATUSFLAG.COMMENTED:
-  //       indicator = 'commented';
-  //       break;
-  //     case INVOICESTATUSFLAG.FLAGGED_MANUALLY:
-  //       indicator = 'flagged';
-  //       break;
-  //     case INVOICESTATUSFLAG.FLAGGED_AUTOMATICALLY:
-  //       indicator = 'flagged';
-  //       break;
-  //     case INVOICESTATUSFLAG.RECLAMATIONS_SENT:
-  //       indicator = 'email';
-  //       break;
-  //     case INVOICESTATUSFLAG.ACCEPTED:
-  //       indicator = 'ok';
-  //       break;
-  //     case INVOICESTATUSFLAG.HOLD:
-  //       indicator = 'inClarification';
-  //       break;
-  //     case INVOICESTATUSFLAG.REJECTED:
-  //       indicator = 'rejected';
-  //       break;
-  //     case INVOICESTATUSFLAG.INVOICE_INSPECTED:
-  //       indicator = 'inspected';
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //   if (indicator) {
-  //     return (
-  //       <Icon
-  //         key={flag}
-  //         type="indicator"
-  //         name={indicator}
-  //         {...size}
-  //       />
-  //     );
-  //   }
-  //   return null;
-  // }
+  getIconForInvoiceFlag = (flag, size, props = {}) => {
+    let indicator = '';
+
+    switch (flag) {
+      case INVOICE_FLAG.Commented:
+        indicator = INVOICE_FLAG.Commented;
+        break;
+      case INVOICE_FLAG.DueDatePending:
+        indicator = INVOICE_FLAG.DueDatePending;
+        break;
+      case INVOICE_FLAG.DueDateOutdated:
+        indicator = INVOICE_FLAG.DueDateOutdated;
+        break;
+      default:
+        break;
+    }
+
+    if (indicator) {
+      return (
+        <Icon
+          key={flag}
+          type="indicator"
+          name={indicator}
+          {...size}
+          {...props}
+        />
+      );
+    }
+    return null;
+  };
 
   getFormatOptions = (amount, currency) => {
     let digits = 2;
