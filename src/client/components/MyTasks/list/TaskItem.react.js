3fb5ada5400a561020e164354b81f56e676acc41
dzhitomirsky-sc <dzhitomirsky@scand.com>
2017-08-31 14:47:12 +0300

    #104 Added due date related badges.

--- src/client/components/MyTasks/list/TaskItem.react.js
+++ src/client/components/MyTasks/list/TaskItem.react.js
@@ -1,10 +1,31 @@
 import React, { Component, PropTypes } from 'react';
 import UiHelpers from '../helpers/UIHelpers.react';
 import './TaskItem.less';
+import moment from 'moment';
+import { INVOICE_FLAG } from '../constants'
 
 const iconSize = {
   width: 25,
   height: 25,
+};
+
+/**
+ * Returns pending badge in case dueDate == today || dueDate == tomorrow
+ * Return outdated badge in case dueDate < today
+ * Return no badge otherwise
+ * @param dueDate
+ */
+const getDueDateBadge = (dueDate) => {
+  const today = moment();
+  const momentDueDate = moment(dueDate);
+
+  if (momentDueDate.isSame(today, 'day') || momentDueDate.isSame(today.add(1, 'days'), 'day')) {
+    return UiHelpers.getIconForInvoiceFlag(INVOICE_FLAG.DueDatePending, iconSize, {style: {fill: 'orange'}});
+  } else if (momentDueDate.isBefore(today, 'day')) {
+    return UiHelpers.getIconForInvoiceFlag(INVOICE_FLAG.DueDateOutdated, iconSize, {style: {fill: 'red'}});
+  } else {
+    return null
+  }
 };
 
 const TaskItem = ({ invoice }, { i18n }) => (
@@ -24,7 +45,9 @@ const TaskItem = ({ invoice }, { i18n }) => (
     </div>
     <div className="list-item-wide-column">
       <div className="list-item">
-        <span className="value">{i18n.formatDate(invoice.dueDate)}</span>
+        <span className="value">
+          {i18n.formatDate(invoice.dueDate)}{' '}{getDueDateBadge(invoice.dueDate)}
+        </span>
       </div>
       <div className="divider"/>
       <span className="value">
@@ -39,7 +62,7 @@ const TaskItem = ({ invoice }, { i18n }) => (
     </div>
     <div className="list-item-narrow-column">
       <div className="list-item">
-        {invoice.commentary ? UiHelpers.getIconForApprovalStatus('commented', iconSize) : ''}
+        {invoice.commentary ? UiHelpers.getIconForInvoiceFlag(INVOICE_FLAG.Commented, iconSize) : ''}
       </div>
       <div className="divider"/>
       <div className="list-item">
